c556f845a37767c44f7deaa859764377e688d160
Ricardo-M-Lu <wn1014332658@gmail.com>
2017-02-11 22:48:44 +0800

    first commit

---
+++ app/src/main/java/cn/labmen/rice_margin_sushi/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.labmen.rice_margin_sushi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
